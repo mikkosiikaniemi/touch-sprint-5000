efca7db796d306771aece3a1c0ad2c3dcb971670
Mikko Siikaniemi <mikko.siikaniemi@vincit.fi>
2023-10-07 16:57:37 +0300

    Define separate drag functions for fingers

--- touchsprint.js
+++ touchsprint.js
@@ -19,13 +19,14 @@ let initialPositionX2, initialPositionY2;
 let totalDistance = 0;
 
 // Add mouse and touch event listeners
-finger1.addEventListener("mousedown", startDrag1);
+//finger1.addEventListener("mousedown", startDrag1);
 finger1.addEventListener("touchstart", startDrag1);
-finger2.addEventListener("mousedown", startDrag2);
+//finger2.addEventListener("mousedown", startDrag2);
 finger2.addEventListener("touchstart", startDrag2);
 
-document.addEventListener("mousemove", drag);
-document.addEventListener("touchmove", drag);
+//document.addEventListener("mousemove", drag);
+finger1.addEventListener("touchmove", drag1);
+finger2.addEventListener("touchmove", drag2);
 
 //document.addEventListener("mouseup", endDrag);
 finger1.addEventListener("touchend", endDrag1);
@@ -81,71 +82,85 @@ function startDrag2(e) {
 	finger2.style.zIndex = "1000";
 }
 
-function drag(e) {
-	if (isDragging1 || isDragging2) {
+function drag1(e) {
+	if (!isDragging1) return;
 
-		// Handle both mouse and touch events
-		let clientX, clientY;
-		if (e.type === "mousemove") {
-			clientX = e.clientX;
-			clientY = e.clientY;
-		} else if (e.type === "touchmove") {
-			const touch = e.touches[0];
-			clientX = touch.clientX;
-			clientY = touch.clientY;
-		}
-
-		// Calculate the new position and distance for draggableElement1
-		if (isDragging1) {
-			const newY1 = clientY - offsetY1;
-			const newX1 = clientX - offsetX1;
-
-			// Calculate the distance moved during this drag
-			let distanceY1 = newY1 - initialPositionY1;
-
-			// Deduct the original elemtent distance from the top of the viewport
-			if (distanceY1 === newY1) {
-				distanceY1 -= originalDistanceFromTop1;
-			}
-
-			// Update the element's position based on the mouse/touch position and offset
-			finger1.style.left = newX1 + "px";
-			finger1.style.top = newY1 + "px";
-
-			// Update the total distance
-			totalDistance += distanceY1;
-
-			initialPositionY1 = newY1;
-		}
-
-		// Calculate the new position and distance for draggableElement1
-		if (isDragging2) {
-			const newY2 = clientY - offsetY2;
-			const newX2 = clientX - offsetX2;
-
-			// Calculate the distance moved during this drag
-			let distanceY2 = newY2 - initialPositionY2;
-
-			// Deduct the original elemtent distance from the top of the viewport
-			if (distanceY2 === newY2) {
-				distanceY2 -= originalDistanceFromTop2;
-			}
-
-			// Update the element's position based on the mouse/touch position and offset
-			finger2.style.left = newX2 + "px";
-			finger2.style.top = newY2 + "px";
-
-			// Update the total distance
-			totalDistance += distanceY2;
-
-			initialPositionY2 = newY2;
-		}
-
-		// Update the background position to move it together with the element
-		body.style.backgroundPositionY = totalDistance + "px";
-
-		distanceDisplay.textContent = totalDistance.toFixed(0);
+	let clientX, clientY;
+	if (e.type === "mousemove") {
+		clientX = e.clientX;
+		clientY = e.clientY;
+	} else if (e.type === "touchmove") {
+		const touch = e.touches[0];
+		clientX = touch.clientX;
+		clientY = touch.clientY;
 	}
+
+	// Calculate the new position and distance for draggableElement1
+	const newY1 = clientY - offsetY1;
+	const newX1 = clientX - offsetX1;
+
+	// Calculate the distance moved during this drag
+	let distanceY1 = newY1 - initialPositionY1;
+
+	// Deduct the original elemtent distance from the top of the viewport
+	if (distanceY1 === newY1) {
+		distanceY1 -= originalDistanceFromTop1;
+	}
+
+	// Update the element's position based on the mouse/touch position and offset
+	finger1.style.left = newX1 + "px";
+	finger1.style.top = newY1 + "px";
+
+	// Update the total distance
+	totalDistance += distanceY1;
+
+	initialPositionY1 = newY1;
+
+	// Update the background position to move it together with the element
+	body.style.backgroundPositionY = totalDistance + "px";
+
+	distanceDisplay.textContent = totalDistance.toFixed(0);
+}
+
+function drag2(e) {
+	if (!isDragging2) return;
+
+	let clientX, clientY;
+	if (e.type === "mousemove") {
+		clientX = e.clientX;
+		clientY = e.clientY;
+	} else if (e.type === "touchmove") {
+		const touch = e.touches[0];
+		clientX = touch.clientX;
+		clientY = touch.clientY;
+	}
+
+	// Calculate the new position and distance for draggableElement1
+
+	const newY2 = clientY - offsetY2;
+	const newX2 = clientX - offsetX2;
+
+	// Calculate the distance moved during this drag
+	let distanceY2 = newY2 - initialPositionY2;
+
+	// Deduct the original elemtent distance from the top of the viewport
+	if (distanceY2 === newY2) {
+		distanceY2 -= originalDistanceFromTop2;
+	}
+
+	// Update the element's position based on the mouse/touch position and offset
+	finger2.style.left = newX2 + "px";
+	finger2.style.top = newY2 + "px";
+
+	// Update the total distance
+	totalDistance += distanceY2;
+
+	initialPositionY2 = newY2;
+
+	// Update the background position to move it together with the element
+	body.style.backgroundPositionY = totalDistance + "px";
+
+	distanceDisplay.textContent = totalDistance.toFixed(0);
 }
 
 function endDrag1() {
